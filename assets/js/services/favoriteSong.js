d93915a7654c12a5037f903aa9add2d51b6e4bec
Mac Van Thanh <macthanhkmhd2003@gmail.com>
2024-05-17 23:38:30 +0700

    17-05(9)

--- assets/js/services/favoriteSong.js
+++ assets/js/services/favoriteSong.js
@@ -1,8 +1,10 @@
 import { users } from "../data/users.js";
 import { songs } from "../data/songs.js";
-
 import { getCookie } from "../helpers/cookies.js";
+import { getAverageColor } from "../helpers/getAverageColor.js";
 // console.log(users);
+
+const $$ = document.querySelectorAll.bind(document);
 
 // get id from cookie
 const userId = getCookie("accessToken");
@@ -227,22 +229,120 @@ const recentPlayed = {
 </div>`;
     trending_container.innerHTML += html;
   },
-  handle: function () {
-    const audio = document.getElementById("audio");
-    const playBtn = document.querySelector(".fa-play");
-    const pauseBtn = document.querySelector(".fa-pause");
 
-    playBtn.addEventListener("click", () => {
-      audio.play();
+  handlePlayMusic: () => {
+    // const trendingSection = $("#treding_container");
+    // const btnHandleMusic = $$(".btnHandleMusic");
+    const songItems = $$(
+      "#treding_container .card-group-grid .card-playing-horizontal"
+    );
+
+    // console.log(songItems);
+    const audio = $$(".card-playing-horizontal #audio");
+    // console.log(audio);
+    // console.log(Array.from(audio));
+    const playBtn = $$(".btnHandleMusic .fa-play");
+    const pauseBtn = $$(".btnHandleMusic .fa-pause");
+    const imageElement = $$(".card-playing-horizontal-header img");
+    // console.log(imageElement);
+    const likeBtn = $$(".likeMusic");
+    const downloadBtn = $$(".downloadMusic");
+    let cur_index;
+    songItems.forEach((song, index) => {
+      song.addEventListener("click", (e) => {
+        // console.log(imageElement[index]);
+        const clickedSong = e.currentTarget;
+        // console.log(clickedSong);
+        const otherSongs = Array.from(songItems).filter(
+          (item) => item !== clickedSong
+        );
+
+        if (
+          !e.target.classList.contains("fa-heart") &&
+          !e.target.classList.contains("fa-download")
+        ) {
+          const imgElement = new Image();
+          if (imgElement) {
+            // console.log(imgElement);
+            imgElement.src = imageElement[index].src;
+            imgElement.setAttribute("crossOrigin", "anonymous");
+            imgElement.onload = function () {
+              const { R, G, B } = getAverageColor(imgElement, 4);
+              // console.log(R, G, B);
+              const color = `rgb(${R}, ${G}, ${B})`;
+              song.style.backgroundColor = color;
+            };
+          }
+          const audioElement = audio[index];
+          const currentPlayingAudio = document.querySelector(".playing audio");
+          cur_index = Array.from(audio).indexOf(currentPlayingAudio);
+          // console.log(cur_index);
+          if (currentPlayingAudio && currentPlayingAudio !== audioElement) {
+            try {
+              currentPlayingAudio.pause();
+              currentPlayingAudio.currentTime = 0;
+              currentPlayingAudio.parentElement.classList.remove("playing");
+              const index = Array.from(audio).indexOf(currentPlayingAudio);
+              playBtn[index].style.display = "inline-block";
+              pauseBtn[index].style.display = "none";
+            } catch (err) {
+              audioElement.pause();
+            }
+          }
+
+          song.classList.toggle("playing");
+
+          if (audioElement.paused) {
+            audioElement.play();
+            playBtn[index].style.display = "none";
+            pauseBtn[index].style.display = "inline-block";
+          } else {
+            audioElement.pause();
+            playBtn[index].style.display = "inline-block";
+            pauseBtn[index].style.display = "none";
+          }
+          // HandleEvents
+          audioElement.addEventListener("ended", () => {
+            song.classList.remove("playing");
+            playBtn[index].style.display = "inline-block";
+            pauseBtn[index].style.display = "none";
+          });
+        }
+      });
     });
 
-    pauseBtn.addEventListener("click", () => {
-      audio.pause();
+    // Like Music
+
+    likeBtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        btn.classList.toggle("active");
+      });
+    });
+
+    // Download Music
+
+    downloadBtn.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        // Tải xuống tệp MP3
+        const currentAudio = audio[index];
+        const audioSource = audioElement.src;
+        const downloadLink = document.createElement("a");
+        // gán đường link
+        downloadLink.href = audioSource;
+        // xét thuộc tính cho thẻ a
+        downloadLink.setAttribute("target", "_blank");
+        downloadLink.setAttribute("rel", "noopener noreferrer");
+        downloadLink.setAttribute("download", `${songs[index].title}`);
+        // thêm vào body
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+      });
     });
   },
   start: function () {
     this.render();
-    // this.handle();
+    this.handlePlayMusic();
   },
 };
 
